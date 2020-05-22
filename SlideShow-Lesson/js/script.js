1f42b81e3489657517eb86b7557ce2a75d0c2742
中村倫士 <nori@nmbp.local>
2020-05-23 04:03:15 +0900

    [Finish]6-8

---
+++ SlideShow-Lesson/js/script.js
@@ -0,0 +1,12 @@
+$('.theTarget').skippr({
+	transition: 'fade',
+	speed: 1000,
+	easing : 'easeOutBounce',
+	navType: 'bubble',
+	childrenElementType : 'div',
+	arrows : true,
+	autoPlay : false,
+	autoPlayDuration : 5000,
+	keyboardOnAlways : true,
+	hidePrevious : false
+});
